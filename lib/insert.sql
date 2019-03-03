db4a2396a9975ff51695004e382b09c9201e1dd3
Naomi Katz <naykatz@gmail.com>
2019-03-03 17:18:25 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears
+VALUES
+  (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1),
+  (2,"Rowdy", 10, "M", "black", "intense", 1),
+  (3,"Tabitha", 6, "F", "dark brown", "Nice", 0),
+  (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 1),
+  (5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+  (6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+  (7,"Wendy", 12, "F", "Blue", "naive", 0),
+  (8,null, 20, "M", "black", "aggressive", 0);
